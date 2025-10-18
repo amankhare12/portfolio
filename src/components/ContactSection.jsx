157883372fc2ccede2cacf47e20745d6590c6606
amankhare12 <amankhare324@gmail.com>
2025-10-18 17:10:43 +0530

    add project

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -7,7 +7,7 @@ const ContactSection = () => {
             id="contact"
             className="py-24 px-4 relative bg-secondary/30"
         >
-            <div className="container mx-auto max-w-7xl">
+            <div className="container mx-auto max-w-6xl">
                 <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
                     Get In <span className="text-primary"> Touch</span>
                 </h2>
